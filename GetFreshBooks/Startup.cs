8c5b3ae23aa91e392673477b29254ed7abc93a25
Su Su Aung <susu.utycc@gmail.com>
2018-05-21 16:09:56 +0800

    making new single GetFreshBooks

---
+++ GetFreshBooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetFreshBooks.Startup))]
+namespace GetFreshBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
