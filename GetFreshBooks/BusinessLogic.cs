1fd0319caf4cda5869042ac5369d7f8be096f719
Gabriel Wong <gabrielwong1988@gmail.com>
2018-05-21 17:30:23 +0800

    Created MainPage (Index.cshtml)

---
+++ GetFreshBooks/BusinessLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetFreshBooks
+{
+    public class BusinessLogic
+    {
+        Mybooks context = new Mybooks();
+
+        public List<Book> GetAll
+        {
+            get
+            {
+                return context.Books.ToList<Book>();
+            }
+        }
+    }
+}
