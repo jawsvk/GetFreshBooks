7a21bbf92b8cf11a935769480168643829679cf8
weinchester <weinchester@gmail.com>
2018-05-21 16:25:33 +0800

    part 1-view data complete

--- GetFreshBooks/App_Start/MVCGridConfig.cs
+++ GetFreshBooks/App_Start/MVCGridConfig.cs
@@ -7,42 +7,74 @@ namespace GetFreshBooks
     using System.Web.Mvc;
     using System.Linq;
     using System.Collections.Generic;
-
     using MVCGrid.Models;
     using MVCGrid.Web;
-
+    using Models;
     public static class MVCGridConfig 
     {
+
         public static void RegisterGrids()
         {
-            /*
-            MVCGridDefinitionTable.Add("UsageExample", new MVCGridBuilder<YourModelItem>()
+            GridDefaults defaultSet1 = new GridDefaults()
+            {
+                Paging = true,
+                ItemsPerPage = 20,
+                Sorting = true,
+                NoResultsMessage = "Sorry, no results were found"
+            };
+
+
+            MVCGridDefinitionTable.Add("Inventory", new MVCGridBuilder<Book>()
                 .WithAuthorizationType(AuthorizationType.AllowAnonymous)
                 .AddColumns(cols =>
                 {
                     // Add your columns here
-                    cols.Add().WithColumnName("UniqueColumnName")
-                        .WithHeaderText("Any Header")
-                        .WithValueExpression(i => i.YourProperty); // use the Value Expression to return the cell text for this column
-                    cols.Add().WithColumnName("UrlExample")
-                        .WithHeaderText("Edit")
-                        .WithValueExpression((i, c) => c.UrlHelper.Action("detail", "demo", new { id = i.Id }));
+                    cols.Add("BookID").WithColumnName("Book_ID")
+                        .WithHeaderText("Book ID Number")
+                        .WithValueExpression(i => i.BookID.ToString()); // use the Value Expression to return the cell text for this column
+                    cols.Add("Title").WithColumnName("Book_Title")
+                        .WithHeaderText("Book Title")
+                        .WithValueExpression((i => i.Title));
+                    cols.Add("Author").WithColumnName("Book_Author")
+                      .WithHeaderText("Author")
+                      .WithValueExpression((i => i.Author));
+                    cols.Add("ISBN").WithColumnName("Book_ISBN")
+                     .WithHeaderText("ISBN")
+                     .WithValueExpression((i => i.ISBN));
+                    cols.Add("Stock").WithColumnName("Book_Stock").WithSorting(true)
+                  .WithHeaderText("Stock Number")
+                  .WithValueExpression((i => i.Stock.ToString()));
+                    cols.Add("Price").WithColumnName("Book_Price")
+                  .WithHeaderText("Price").WithValueExpression((i => i.Price.ToString("C2")));
+                    cols.Add("Edit").WithHtmlEncoding(false)
+                    .WithSorting(false)
+                    .WithHeaderText(" ")
+                    .WithValueTemplate("<a href='{Value}' class='btn btn-primary' role='button'>Edit</a>");
+                    cols.Add("Delete").WithHtmlEncoding(false)
+                        .WithSorting(false)
+                        .WithHeaderText(" ")
+                        .WithValueTemplate("<a href='{Value}' class='btn btn-danger' role='button'>Delete</a>");
+                  
+
                 })
                 .WithRetrieveDataMethod((context) =>
                 {
                     // Query your data here. Obey Ordering, paging and filtering parameters given in the context.QueryOptions.
                     // Use Entity Framework, a module from your IoC Container, or any other method.
                     // Return QueryResult object containing IEnumerable<YouModelItem>
+                    var result = new QueryResult<Book>();
+                    result.Items = new BookshopEntities().Books.ToList();
+                    return result;
+                    //return new QueryResult<Book>()
+                    //{
+                    //    Items = new List<Book>(),
+                        
+                    //    TotalRecords = 0 //if paging is enabled, return the total number of all pages
 
-                    return new QueryResult<YourModelItem>()
-                    {
-                        Items = new List<YourModelItem>(),
-                        TotalRecords = 0 // if paging is enabled, return the total number of records of all pages
-                    };
-
+                    //};
                 })
             );
-            */
+            
         }
     }
 }
