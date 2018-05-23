fbec289eb7be8affdbbcafd89caf983bd79ba6d6
Su Su Aung <susu.utycc@gmail.com>
2018-05-23 11:35:12 +0800

    add to cart alert

--- GetFreshBooks/Controllers/HomeController.cs
+++ GetFreshBooks/Controllers/HomeController.cs
@@ -8,12 +8,14 @@ namespace GetFreshBooks.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Categories = BusinessLogic.GetAllCategories;
             ViewBag.Books = BusinessLogic.GetAllBooks;
             return View();
         }
+
         [HttpPost]
         public ActionResult Index(string isbn)
         {
@@ -21,10 +23,10 @@ namespace GetFreshBooks.Controllers
             BusinessLogic.AddToCart(isbn);
             ViewBag.Categories = BusinessLogic.GetAllCategories;
             ViewBag.Books = BusinessLogic.GetAllBooks;
+            ViewData["addedToCart"] = true;
             return View("Index");         
         }
 
-        // [Authorize(Roles ="User,Admin")]
         public ActionResult About()
         {
             ViewBag.Message = "GetFreshBooks";
